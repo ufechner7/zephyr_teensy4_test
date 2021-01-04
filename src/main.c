8db3b6b97dbbee6c6d4d9d27c1ca5457eddb4e5a
Uwe Fechner <uwe.fechner@nl.abb.com>
2021-01-04 11:10:22 +0100

    Fixed whitespace.

--- src/main.c
+++ src/main.c
@@ -31,27 +31,27 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL_DBG);
 
 void main(void)
 {
-	const struct device *usb;
-	uint32_t version = sys_kernel_version_get();
+    const struct device *usb;
+    uint32_t version = sys_kernel_version_get();
 
-	usb = device_get_binding(CONFIG_UART_CONSOLE_ON_DEV_NAME);
-	if (usb == NULL) {
-		return;
-	}
+    usb = device_get_binding(CONFIG_UART_CONSOLE_ON_DEV_NAME);
+    if (usb == NULL) {
+        return;
+    }
 
-	log_init();
-	if (usb_enable(NULL)) {
-		return;
-	}
+    log_init();
+    if (usb_enable(NULL)) {
+        return;
+    }
 
-	// sleep some time after boot to give USB-Client a chance to connect before first log output is produced
-	k_msleep(START_DELAY);
-	LOG_INF("Started zephyr %u.%u.%u on board %s.", SYS_KERNEL_VER_MAJOR(version), SYS_KERNEL_VER_MINOR(version), SYS_KERNEL_VER_PATCHLEVEL(version), CONFIG_BOARD);
+    // sleep some time after boot to give USB-Client a chance to connect before first log output is produced
+    k_msleep(START_DELAY);
+    LOG_INF("Started zephyr %u.%u.%u on board %s.", SYS_KERNEL_VER_MAJOR(version), SYS_KERNEL_VER_MINOR(version), SYS_KERNEL_VER_PATCHLEVEL(version), CONFIG_BOARD);
 
     // this loop is needed to define how often log messages are processed
-	while (true) {
+    while (true) {
         if (log_process(false) == false) {
             k_msleep(30);   /* sleep 30 ms*/
         }
-     }
+    }
 }
